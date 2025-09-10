07a87a99f6c7f8aef44990102f9ac7dd1bf880f2
R4v3ns <136044464+R4v3ns@users.noreply.github.com>
2025-09-10 16:06:11 -0300

    feat new route
    se agrego nueva ruta del perfil

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,5 +7,6 @@ const { register } = require("../controllers/user.controller")
 
 // Register Route
 router.post("/register", register)
+router.post("/profile/:id", viewProfile)
 
 module.exports = router
